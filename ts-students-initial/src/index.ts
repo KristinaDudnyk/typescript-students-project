a67cf7231d065894352239c41b5bf1440f2b40e2
Kristina <kristina.dudnyk20@gmail.com>
2024-05-31 19:44:56 +0100

    initial commit, added JS code, seted up TS

---
+++ ts-students-initial/src/index.ts
@@ -0,0 +1,27 @@
+import data from "./data.js";
+
+const students = JSON.parse(data);
+console.log(students);
+
+function addRow(table) {
+  let tr = table.querySelector("tbody").insertRow();
+  const name = tr.insertCell();
+  name.appendChild(document.createTextNode("Elena Garcia"));
+
+  const age = tr.insertCell();
+  age.appendChild(document.createTextNode("33"));
+
+  const majors = tr.insertCell();
+  majors.appendChild(
+    document.createTextNode("Space Engineering, Civil Engineering")
+  );
+
+  const status = tr.insertCell();
+  status.appendChild(document.createTextNode("Active"));
+}
+
+function selectTable() {
+  return document.querySelector("#students-table");
+}
+
+addRow(selectTable());
